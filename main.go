85e794cef230a807cbe5c4b2c884d9748083710d
Antonio José Peleteiro Crespo <antonio.peleteiro@cabify.com>
2023-12-11 13:35:13 +0100

    Routers and 3rd Party Libraries

--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func contactHandler(w http.ResponseWriter) {
+func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	_, _ = fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
 }
 
-func faqHandler(w http.ResponseWriter) {
+func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	_, _ = fmt.Fprint(w, `<h1>FAQ Page</h1>
 <ul>
@@ -36,25 +38,17 @@ func faqHandler(w http.ResponseWriter) {
 `)
 }
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w)
-	case "/contact":
-		contactHandler(w)
-	case "/faq":
-		faqHandler(w)
-	default:
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-	}
-}
-
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
-	err := http.ListenAndServe(":3000", router)
+	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		fmt.Println(err)
 		return
